efe9e8ccb82a3454123db2a30628c6b8bf66f971
edgardo-mkr <gonzaedgardo20@gmail.com>
2021-08-18 21:44:39 -0400

    adding comments on contracts

--- contracts/SwapV1.sol
+++ contracts/SwapV1.sol
@@ -19,35 +19,44 @@ contract SwapV1 is Initializable {
     // arr[0] = Dai, arr[1] = Link, arr[2] = Uni. THE ORDER MATTERS!!!!! e.g: [20,50,30] convert 20% to dai, 50% to Link and 30% to Uni.
     //if there is a token that is not desired to convert it must be set to "0". 
     function swapTokens(uint[3] memory _amountOfTokens) public payable returns(bool){
-        require(_amountOfTokens[0]>=0 && _amountOfTokens[1]>=0 && _amountOfTokens[2]>=0, "Negative percentages not allow");
-        require(_amountOfTokens[0]+_amountOfTokens[1]+_amountOfTokens[2] == 100, "Error in percentages of required tokens");//checking that the porcentages sum 100
-        require(msg.value > 0, "No Ether has been sent");
+        require(_amountOfTokens[0]>=0 && _amountOfTokens[1]>=0 && _amountOfTokens[2]>=0, "Negative percentages not allow"); //checking for negative inputs
+        require(_amountOfTokens[0]+_amountOfTokens[1]+_amountOfTokens[2] == 100, "Error in percentages of required tokens");//checking that the percentages sum 100
+        require(msg.value > 0, "No Ether has been sent");//this function will revert if no ether is sent
+
         uint finalAmount = msg.value - (msg.value/1000);//substracting the fee from the deposited amount
         uint daiAmount = (finalAmount * _amountOfTokens[0])/100; //calculating the diferent amounts to swap for each token
         uint linkAmount = (finalAmount * _amountOfTokens[1])/100;//...
         uint uniAmount = (finalAmount * _amountOfTokens[2])/100;//...
         uint[] memory expectedAmount = new uint[](2);
-        address[] memory path = new address[](2);
-        path[0] = uniSwap.WETH();
+        address[] memory path = new address[](2);//stores the token contract addresses desired to convert
+        path[0] = uniSwap.WETH();//setting input token to WETH
         
         console.log("owner balance: %s", owner.balance);
+
         payable(owner).call{value: msg.value/1000}("");//transfering the fee to the recipient (owner)
         
         console.log("trying to send %s ETH to owner", msg.value/1000);
         console.log("owner's new balance: %s", owner.balance);
-        if(_amountOfTokens[0] > 0){
-            path[1] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;//Dai contract address
-            expectedAmount = uniSwap.getAmountsOut(daiAmount,path);
+
+        if(_amountOfTokens[0] > 0){ //if percentage is "0" ommit this swap
+
+            path[1] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;//setting the output token to the Dai contract address
+
+            expectedAmount = uniSwap.getAmountsOut(daiAmount,path);//calculating minimum amount of token to receive 
             uniSwap.swapExactETHForTokens{value: daiAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
         }
-        if(_amountOfTokens[1] > 0){
-            path[1] = 0x514910771AF9Ca656af840dff83E8264EcF986CA;//Link contract address
-            expectedAmount = uniSwap.getAmountsOut(linkAmount,path);
+        if(_amountOfTokens[1] > 0){ //if percentage is "0" ommit this swap
+
+            path[1] = 0x514910771AF9Ca656af840dff83E8264EcF986CA;//setting the output token to the Link contract address
+
+            expectedAmount = uniSwap.getAmountsOut(linkAmount,path);//calculating minimum amount of token to receive
             uniSwap.swapExactETHForTokens{value: linkAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
         }
-        if(_amountOfTokens[2] > 0){
-            path[1] = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;//Uni contract address
-            expectedAmount = uniSwap.getAmountsOut(uniAmount,path);
+        if(_amountOfTokens[2] > 0){ //if percentage is "0" ommit this swap
+
+            path[1] = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;//setting the output token to the Uni contract address
+
+            expectedAmount = uniSwap.getAmountsOut(uniAmount,path);//calculating minimum amount of token to receive
             uniSwap.swapExactETHForTokens{value: uniAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
         }
         return true;
--- contracts/SwapV2.sol
+++ contracts/SwapV2.sol
@@ -8,7 +8,7 @@ import "./interfaces/Initializable.sol";
 contract SwapV2 is Initializable {
     address public owner;
     IUniswapV2Router uniSwap;
-    IUniswapV2Router sushiSwap;
+    IUniswapV2Router sushiSwap;//adding second DEX, sushiSwap
 
     // in order to make the contract upgradable an initializer function is used instead of the constructor
     function initialize(address _owner) public initializer{
@@ -16,27 +16,34 @@ contract SwapV2 is Initializable {
         uniSwap = IUniswapV2Router(0x7a250d5630B4cF539739dF2C5dAcb4c659F2488D);// pointing to the uniSwapV2Router contract
     }
 
+    //this takes care of setting the new interface storage variable for sushiswap
     function setSushiSwap() external{
         require(msg.sender == owner);
         sushiSwap = IUniswapV2Router(0xd9e1cE17f2641f24aE83637ab66a2cca9C378B9F);// pointing to the sushiSwapV2Router contract, which is the exact same implementation of the UniswapV2Router
     }
 
+    //in difference with SwapV1, this implementation receives another input called _protocol 
+    //_protocol is a string array with the name of the protocol to use for each corresponding token  
     function swapTokens(string[3] memory _protocols, uint[3] memory _amountOfTokens) external payable returns(bool){
-        require(_amountOfTokens[0]>=0 && _amountOfTokens[1]>=0 && _amountOfTokens[2]>=0, "Negative percentages not allow");
-        require(_amountOfTokens[0]+_amountOfTokens[1]+_amountOfTokens[2] == 100, "Error in percentages of required tokens");//checking that the porcentages sum 100
-        require(msg.value > 0, "No Ether has been sent");
+        require(_amountOfTokens[0]>=0 && _amountOfTokens[1]>=0 && _amountOfTokens[2]>=0, "Negative percentages not allow"); //checking for negative inputs
+        require(_amountOfTokens[0]+_amountOfTokens[1]+_amountOfTokens[2] == 100, "Error in percentages of required tokens");//checking that the percentages sum 100
+        require(msg.value > 0, "No Ether has been sent");//this function will revert if no ether is sent
+
         uint finalAmount = msg.value - (msg.value/1000);//substracting the fee from the deposited amount
         uint daiAmount = (finalAmount * _amountOfTokens[0])/100; //calculating the diferent amounts to swap for each token
         uint linkAmount = (finalAmount * _amountOfTokens[1])/100;//...
         uint uniAmount = (finalAmount * _amountOfTokens[2])/100;//...
         uint[] memory expectedAmount = new uint[](2);
-        address[] memory path = new address[](2);
-        path[0] = uniSwap.WETH();
+        address[] memory path = new address[](2);//stores the token contract addresses desired to convert
+        path[0] = uniSwap.WETH();//setting input token to WETH
         
         payable(owner).call{value: msg.value/1000}("");//transfering the fee to the recipient (owner)
         
-        if(_amountOfTokens[0] > 0){
-            path[1] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;//Dai contract address
+        if(_amountOfTokens[0] > 0){//if percentage is "0" ommit this swap
+            path[1] = 0x6B175474E89094C44Da98b954EedeAC495271d0F;//setting the output token to the Dai contract address
+
+            //confirming which DEX was chosen to make the right swap 
+            //if neither was chosen or there was a mispelling, a revert is called
             if(keccak256(abi.encodePacked(_protocols[0]))==keccak256(abi.encodePacked("SUSHI"))){
                 expectedAmount = sushiSwap.getAmountsOut(daiAmount,path);
                 sushiSwap.swapExactETHForTokens{value: daiAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
@@ -46,9 +53,11 @@ contract SwapV2 is Initializable {
             } else {
                 revert("Unsupported DEX");
             }
+
         }
-        if(_amountOfTokens[1] > 0){
-            path[1] = 0x514910771AF9Ca656af840dff83E8264EcF986CA;//Link contract address
+        if(_amountOfTokens[1] > 0){//if percentage is "0" ommit this swap
+            path[1] = 0x514910771AF9Ca656af840dff83E8264EcF986CA;//setting the output token to the Link contract address
+
             if(keccak256(abi.encodePacked(_protocols[1]))==keccak256(abi.encodePacked("SUSHI"))){
                 expectedAmount = sushiSwap.getAmountsOut(linkAmount,path);
                 sushiSwap.swapExactETHForTokens{value: linkAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
@@ -58,9 +67,11 @@ contract SwapV2 is Initializable {
             } else{
                 revert("Unsupported DEX");
             }
+
         }
-        if(_amountOfTokens[2] > 0){
-            path[1] = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;//Uni contract address
+        if(_amountOfTokens[2] > 0){//if percentage is "0" ommit this swap
+            path[1] = 0x1f9840a85d5aF5bf1D1762F925BDADdC4201F984;//setting the output token to the Uni contract address
+
             if(keccak256(abi.encodePacked(_protocols[2]))==keccak256(abi.encodePacked("SUSHI"))){
                 expectedAmount = sushiSwap.getAmountsOut(uniAmount,path);
                 sushiSwap.swapExactETHForTokens{value: uniAmount}(expectedAmount[1],path,msg.sender,block.timestamp + 15);
@@ -70,6 +81,7 @@ contract SwapV2 is Initializable {
             }else {
                 revert("Unsupported DEX");
             }
+
         }
         return true;
     }
